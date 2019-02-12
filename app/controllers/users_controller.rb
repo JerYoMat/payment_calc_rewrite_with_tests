074049831c919030ac0c5fe6a88cedda03576ebe
JerYoMat <jeremy.emata@gmail.com>
2019-02-12 04:12:02 -0500

    added sign up and logout actions to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
+require 'pry'
 class UsersController < ApplicationController
+
+    get '/signup' do 
+      @user = User.new 
+      erb  :'users/new' 
+    end 
+
+    post '/signup' do 
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        if @user.save
+          session[:user_id] = @user.id 
+          redirect to '/loans'
+        else 
+          erb :'users/new'
+        end 
+    end 
+
+    get '/users' do 
+    end 
+
+    get '/users/:id' do 
+    end 
+
+    get '/users/:id/edit' do 
+    end 
+
+    patch '/users/:id' do 
+    end 
+
+    delete '/users/:id' do 
+    end 
+
 end 
+
