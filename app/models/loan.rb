9bfe92c0a9c0186379c9b5519f54f0d74745eb6e
JerYoMat <jeremy.emata@gmail.com>
2019-02-11 23:38:01 -0500

    added loan model associations and validations

---
+++ app/models/loan.rb
@@ -0,0 +1,12 @@
+require './config/environment'
+class Loan < ActiveRecord::Base 
+  belongs_to :user 
+  
+  validates :loan_face_value, numericality: {greater_than: 0}
+  validates :loan_term, numericality: {greater_than: 0}
+  validates :annual_rate, numericality: {greater_than: 0}
+  validates :annual_rate, numericality: {less_than: 100}
+  validates :total_amount, numericality: {less_than: 100}
+  validates :lender_name, presence: true 
+
+end 
