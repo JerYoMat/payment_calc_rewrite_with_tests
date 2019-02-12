d38ef19aee67e8e1c18f6964fae5c6f3786bc340
JerYoMat <jeremy.emata@gmail.com>
2019-02-12 04:13:00 -0500

    added login and logou actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+
+  get '/login' do 
+    if session[:user_id]
+      redirect to '/loans'
+    else 
+      erb :'sessions/new'
+    end 
+  end 
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/loans'
+    else
+      binding.pry
+      redirect to '/login'
+    end
+  end 
+
+  get '/logout' do
+    if session[:user_id]
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
 end 
