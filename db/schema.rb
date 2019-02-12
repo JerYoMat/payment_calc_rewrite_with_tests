f34624fb5be2e68f12451018843cf17a194eb783
JerYoMat <jeremy.emata@gmail.com>
2019-02-11 23:21:52 -0500

    updated user model to include email

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
+    t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
