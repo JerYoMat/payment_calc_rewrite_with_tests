bcf0f9334a25466d1ed8d355d98dca9fea4a6af1
JerYoMat <jeremy.emata@gmail.com>
2019-02-11 22:09:55 -0500

    added model migrations and migrated db

---
+++ db/migrate/02_create_loans.rb
@@ -0,0 +1,14 @@
+class CreateLoans < ActiveRecord::Migration
+  def change
+    create_table :loans do |t|
+      t.belongs_to :user
+      t.float :loan_face_value
+      t.float :loan_term
+      t.float :annual_rate
+      t.float :total_amount
+      t.string :lender_name 
+      t.timestamps
+    end
+  end
+
+end
